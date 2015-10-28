7346985567bfb2f2d36dbbe11ed255f7a4cd592a
Bowes <bowes@amazon.com>
2015-10-28 10:39:33 +0200

    Some racer fixes, add rustfmt

--- marc/configure-rust.el
+++ marc/configure-rust.el
@@ -1,6 +1,14 @@
 ;; racer -- completion
 (starter-kit-install-if-needed 'company)
 (setq racer-rust-src-path (concat (getenv "HOME") "/code/rust/rust/src"))
-(setq racer-cmd (concat (getenv "HOME") "/code/rust/racer/target/racer"))
-(add-to-list 'load-path (concat (getenv "HOME") "/code/rust/racer/editors"))
+(setq racer-cmd (concat (getenv "HOME") "/code/rust/racer/target/release/racer"))
+(add-to-list 'load-path (concat (getenv "HOME") "/code/rust/racer/editors/emacs"))
 (eval-after-load "rust-mode" '(require 'racer))
+(require 'racer)
+(add-hook 'rust-mode-hook #'racer-activate)
+(define-key rust-mode-map (kbd "TAB") #'racer-complete-or-indent)
+(define-key rust-mode-map (kbd "M-.") #'racer-find-definition)
+
+;; installed things on nightly
+(setenv "PATH" (concat (getenv "PATH") ":" (getenv "HOME") "/.multirust/toolchains/nightly/cargo/bin"))
+(add-to-list 'exec-path (concat (getenv "HOME") "/.multirust/toolchains/nightly/cargo/bin"))
