0d34940e83366208c3692acaaebf1beb3cddd765
Bowes <bowes@amazon.com>
2015-12-10 22:02:58 +0200

    Rust stable updates
    Rust 1.5 is out, with `cargo install`. This means both `rustfmt` and
    `racer` are now installed in the stable toolchain.
    
    * The exec-path is now pointed to stable
    * Racer is installed via `cargo install`
    * And racer comes in through the package manager

--- marc/configure-rust.el
+++ marc/configure-rust.el
@@ -1,16 +1,17 @@
 ;; racer -- completion
 (starter-kit-install-if-needed 'company)
-(setq racer-rust-src-path (concat (getenv "HOME") "/code/rust/rust/src"))
-(setq racer-cmd (concat (getenv "HOME") "/code/rust/racer/target/release/racer"))
-(add-to-list 'load-path (concat (getenv "HOME") "/code/rust/racer/editors/emacs"))
+(starter-kit-install-if-needed 'racer)
 (eval-after-load "rust-mode" '(require 'racer))
-(require 'racer)
+
+(setq racer-rust-src-path (concat (getenv "HOME") "/code/rust/rust/src"))
+
+(require 'rust-mode)
 (add-hook 'rust-mode-hook #'racer-activate)
 (define-key rust-mode-map (kbd "TAB") #'racer-complete-or-indent)
 (define-key rust-mode-map (kbd "M-.") #'racer-find-definition)
 
-;; installed things on nightly
+;; installed things on stable
 (starter-kit-install-if-needed 'rustfmt)
 (define-key rust-mode-map (kbd "C-c C-f") #'rustfmt-format-buffer)
-(setenv "PATH" (concat (getenv "PATH") ":" (getenv "HOME") "/.multirust/toolchains/nightly/cargo/bin"))
-(add-to-list 'exec-path (concat (getenv "HOME") "/.multirust/toolchains/nightly/cargo/bin"))
+(setenv "PATH" (concat (getenv "PATH") ":" (getenv "HOME") "/.multirust/toolchains/stable/cargo/bin"))
+(add-to-list 'exec-path (concat (getenv "HOME") "/.multirust/toolchains/stable/cargo/bin"))
