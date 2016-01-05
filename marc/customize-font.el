581d15a78d64e57e3ebd3cdff2e38fb8bb56aff5
Bowes <bowes@amazon.com>
2016-01-05 10:27:07 +0200

    Source Code Pro

---
+++ marc/customize-font.el
@@ -0,0 +1,13 @@
+;; http://www.emacswiki.org/emacs/SetFonts
+
+;; You can get Source Code Pro here:
+;;   https://github.com/adobe-fonts/Source-Code-Pro
+;; To install on OSX:
+;;   - Download and unzip the latest release
+;;   - Select any of the subfolders in Font Book (I used OTF)
+
+(set-face-attribute 'default nil
+                    :family "Source Code Pro"
+                    :height 110
+                    :weight 'normal
+                    :width 'normal)
