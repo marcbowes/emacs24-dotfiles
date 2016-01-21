d4491d268ea11d562b28016c46bfde42f25c81a2
Bowes <bowes@amazon.com>
2016-01-21 20:37:35 +0200

    Bye nxhtml
    1. I don't use erb that much (haml)
    2. It is slow and literally the only thing that ever crashes emacs
    3. The homepage doesn't even exist anymore

--- marc/rails.el
+++ marc/rails.el
@@ -2,16 +2,5 @@
 (starter-kit-install-if-needed 'rinari)
 (require 'rinari)
 
-;; nxhtml
-(load (concat starter-kit-dir "vendor/nxhtml/autostart.el"))
-
-;; html+erb
-(add-to-list 'load-path (concat starter-kit-dir "vendor/nxhtml/util"))
-(starter-kit-install-if-needed 'mumamo)
-(require 'mumamo-fun)
-(setq mumamo-chunk-coloring 'submode-colored)
-(add-to-list 'auto-mode-alist '("\\.rhtml\\'" . eruby-html-mumamo))
-(add-to-list 'auto-mode-alist '("\\.html\\.erb\\'" . eruby-html-mumamo))
-
 ;; scss
 (setq scss-compile-at-save 'nil)
