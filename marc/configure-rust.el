5fab6dc4cd8b304980695a5a1cd1137892d1216f
Marc Bowes <marcbowes@gmail.com>
2016-11-14 14:01:11 +0200

    Use rustup to manage rust-src

--- marc/configure-rust.el
+++ marc/configure-rust.el
@@ -3,7 +3,21 @@
 (starter-kit-install-if-needed 'racer)
 (eval-after-load "rust-mode" '(require 'racer))
 
-(setq racer-rust-src-path (concat (getenv "HOME") "/code/rust/rust/src"))
+;; installed things via cargo, including rustup!
+(setenv "PATH" (concat (getenv "PATH") ":" (getenv "HOME") "/.cargo/bin"))
+(add-to-list 'exec-path (concat (getenv "HOME") "/.cargo/bin"))
+
+;; use rustup to configure the default toolchain
+(require 's)
+(require 'dash)
+(setq rust-default-toolchain
+      (car (s-split " " (-first
+                         (lambda (line) (s-match "default" line)) 
+                         (s-lines (shell-command-to-string "rustup toolchain list"))))))
+
+;; tell racer to use the rustup-managed rust-src
+;; rustup component add rust-src
+(setq racer-rust-src-path (concat (getenv "HOME") "/.multirust/toolchains/" rust-default-toolchain "/lib/rustlib/src/rust/src"))
 
 (require 'rust-mode)
 (add-hook 'rust-mode-hook #'racer-mode)
@@ -16,8 +30,5 @@
 (add-hook 'rust-mode-hook #'flycheck-mode)
 (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
 
-;; installed things on stable
 (starter-kit-install-if-needed 'rustfmt)
 (define-key rust-mode-map (kbd "C-c C-f") #'rustfmt-format-buffer)
-(setenv "PATH" (concat (getenv "PATH") ":" (getenv "HOME") "/.cargo/bin"))
-(add-to-list 'exec-path (concat (getenv "HOME") "/.cargo/bin"))
