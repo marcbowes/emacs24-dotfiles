116537029aa974cad549ff5aa8657dfc56b0e314
Marc Bowes <marcbowes@gmail.com>
2018-10-04 11:30:45 -0700

    Update configure-rust

--- marc/configure-rust.el
+++ marc/configure-rust.el
@@ -1,35 +1,77 @@
 ;; racer -- completion
-(starter-kit-install-if-needed 'company)
-(starter-kit-install-if-needed 'racer)
-(eval-after-load "rust-mode" '(require 'racer))
+;; (starter-kit-install-if-needed 'company)
+;; (starter-kit-install-if-needed 'racer)
+;; (eval-after-load "rust-mode" '(require 'racer))
 
 ;; installed things via cargo, including rustup!
 (setenv "PATH" (concat (getenv "PATH") ":" (getenv "HOME") "/.cargo/bin"))
 (add-to-list 'exec-path (concat (getenv "HOME") "/.cargo/bin"))
 
-;; use rustup to configure the default toolchain
-(when (executable-find "rustup")
-  (starter-kit-install-if-needed 's 'dash)
-  (require 's)
-  (require 'dash)
-  (setq rust-default-toolchain
-        (car (s-split " " (-first
-                           (lambda (line) (s-match "default" line)) 
-                           (s-lines (shell-command-to-string "rustup toolchain list"))))))
-  ;; tell racer to use the rustup-managed rust-src
-  ;; rustup component add rust-src
-  (setq racer-rust-src-path (concat (getenv "HOME") "/.multirust/toolchains/" rust-default-toolchain "/lib/rustlib/src/rust/src")))
+(defun configure-rust/setup-env ()
+  "RLS requires some environment variables to be setup. We use rustup to get the values."
+  
+  (when (executable-find "rustup")
+    (require 's)
+    (require 'dash)
+    (setq rust-default-toolchain
+          (car (s-split " " (-first
+                             (lambda (line) (s-match "default" line))
+                             (s-lines (shell-command-to-string "rustup toolchain list"))))))
+    ;; tell racer to use the rustup-managed rust-src
+    ;; rustup component add rust-src
+    (setq rust-src-path (concat (getenv "HOME") "/.multirust/toolchains/" rust-default-toolchain "/lib/rustlib/src/rust/src"))
+    (setq rust-bin-path (concat (getenv "HOME") "/.multirust/toolchains/" rust-default-toolchain "/bin"))
+    (setq racer-rust-src-path rust-src-path)
+    (setenv "RUST_SRC_PATH" rust-src-path)
+    (setenv "RUSTC" (concat rust-bin-path "/rustc"))
+    (setenv "CARGO_INCREMENTAL" "1")))
 
-(require 'rust-mode)
-(add-hook 'rust-mode-hook #'racer-mode)
-(add-hook 'racer-mode-hook #'eldoc-mode)
-(add-hook 'racer-mode-hook #'company-mode)
-(setq company-tooltip-align-annotations t)
-(define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+(configure-rust/setup-env)
 
-(starter-kit-install-if-needed 'flycheck-rust)
-(add-hook 'rust-mode-hook #'flycheck-mode)
-(add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+(use-package racer
+        :ensure t
+        :after company)
 
-(starter-kit-install-if-needed 'rustfmt)
-(define-key rust-mode-map (kbd "C-c C-f") #'rustfmt-format-buffer)
+(setq configure-rust-use-lsp nil)
+
+(if configure-rust-use-lsp
+    ;; I tried to get the RLS working but could not get
+    ;; xref-find-definitions working ("no result").
+    (progn
+      (use-package lsp-mode)
+
+      (use-package lsp-flycheck
+        :after flycheck)
+
+      (use-package company-lsp)
+
+      (use-package rust-mode
+        :mode "\\.rs\\'" ;; this is already done by rust-mode
+        :after (lsp-rust lsp-flycheck company-lsp)
+        :config
+        (add-hook 'rust-mode-hook #'lsp-rust-enable)
+        (add-hook 'rust-mode-hook #'flycheck-enable))
+      
+      (use-package lsp-rust
+        :after lsp-mode
+        :config
+        (setq lsp-rust-rls-command '("rustup" "run" "nightly" "rls"))))
+
+  ;; Not using RLS
+  (use-package rustfmt)
+  
+  (use-package rust-mode
+    :after rustfmt
+    :config
+    (add-hook 'rust-mode-hook #'racer-mode)
+    (add-hook 'racer-mode-hook #'eldoc-mode)
+    (add-hook 'racer-mode-hook #'company-mode)
+    (setq company-tooltip-align-annotations t)
+
+    :bind (("TAB". company-indent-or-complete-common)
+           ("C-c C-f" . rustfmt-format-buffer)))
+  
+  (use-package flycheck-rust
+    :config
+    (add-hook 'rust-mode-hook #'flycheck-mode)
+    (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)))
